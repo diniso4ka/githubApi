9c8b4ae4b8482db10a61a58caf9a969442f734e3
deniso4ka <den.zagrosh@gmail.com>
2022-09-06 22:49:25 +0300

    fix: loader

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-const resultNumber = document.getElementById('result-count__number')
+const resultCount = document.getElementById('result-count')
 const inputButton = document.getElementById('search-block__input')
 const searchButton = document.getElementById('search-button')
 
@@ -8,7 +8,7 @@ const value = inputButton.value
 
 
 const createCountElement = (data) => {
-   data.total_count ? resultNumber.innerHTML = `Найдено ${data.total_count} результатов` : null;
+   data.total_count ? resultCount.innerHTML = `Найдено ${data.total_count} результатов` : null;
 }
 
 const createLoadingElement = () => {
@@ -23,23 +23,25 @@ const deleteLoadingElement = () => {
 
 const loadItemList = async (searchValue) => {
    try {
+      const resultList = document.getElementById('result-items')
+      const resultCount = document.getElementById('result-count')
+      resultCount.firstChild.remove()
+      resultList.innerHTML = ''
       createLoadingElement()
       const data = await fetch(`https://api.nomoreparties.co/github-search?q=${searchValue}`).then(res => res.json())
       deleteLoadingElement()
-      console.log(data)
-      const resultList = document.getElementById('result-items')
-      resultList.innerHTML = ''
-
-      createCountElement(data)
-      data.items.forEach(item => createItem(item.full_name, item.html_url, item.description))
+      if (data.total_count) {
+         createCountElement(data)
+         data.items.forEach(item => createItem(item.full_name, item.html_url, item.description))
+      } else {
+         createErrorElement('Ничего не найдено.')
+      }
    } catch (error) {
       createErrorElement('Произошла ошибка.')
    }
 }
 
-const loadData = () => {
-   loadItemList(inputButton.value)
-}
+
 
 const createItem = (path, link, desc) => {
    const resultList = document.getElementById('result-items')
@@ -76,9 +78,14 @@ const createItem = (path, link, desc) => {
 
 
 const createErrorElement = (text) => {
+   const content = document.querySelector('.result-container')
    let div = document.createElement('div')
    div.innerHTML = text
-   resultItem.prepend(div)
+   content.prepend(div)
+}
+
+const loadData = () => {
+   loadItemList(inputButton.value)
 }
 
 
@@ -87,5 +94,95 @@ const createErrorElement = (text) => {
 
 searchButton.addEventListener('click', loadData)
 
+
+// const resultNumber = document.getElementById('result-count__number')
+// const inputButton = document.getElementById('search-block__input')
+// const searchButton = document.getElementById('search-button')
+
+
+// let loadingElement = document.createElement('p')
+// const value = inputButton.value
+
+
+// const createCountElement = (data) => {
+//    data.total_count ? resultNumber.innerHTML = `Найдено ${data.total_count} результатов` : null;
+// }
+
+// const createLoadingElement = () => {
+//    const resultList = document.getElementById('result-items')
+//    loadingElement.innerHTML = 'Идёт загрузка'
+//    resultList.append(loadingElement)
+// }
+
+// const deleteLoadingElement = () => {
+//    loadingElement.innerHTML = ''
+// }
+
+// const loadItemList = async (searchValue) => {
+//    try {
+//       createLoadingElement()
+//       const data = await fetch(`https://api.nomoreparties.co/github-search?q=${searchValue}`).then(res => res.json())
+//       deleteLoadingElement()
+//       console.log(data)
+//       const resultList = document.getElementById('result-items')
+//       // resultList.innerHTML = ''
+
+//       createCountElement(data)
+//       data.items.forEach(item => createItem(item.full_name, item.html_url, item.description))
+//    } catch (error) {
+//       createErrorElement('Произошла ошибка.')
+//    }
+// }
+
+// const loadData = () => {
+//    loadItemList(inputButton.value)
+// }
+
+// const createItem = (path, link, desc) => {
+//    const resultList = document.getElementById('result-items')
+
+//    const newItem = document.createElement('div')
+//    newItem.classList.add('item-card')
+
+
+
+//    const itemImage = document.createElement('img')
+//    itemImage.classList.add('item-img')
+//    itemImage.setAttribute('src', 'https://c0.klipartz.com/pngpicture/1019/439/gratis-png-directorio-carpetas-de-archivos-de-iconos-del-ordenador.png')
+//    newItem.append(itemImage)
+
+
+//    const itemText = document.createElement('div')
+//    itemText.classList.add('item-text')
+//    newItem.append(itemText)
+
+//    const itemLink = document.createElement('a')
+//    itemLink.classList.add('item-link')
+//    itemLink.innerHTML = path
+//    itemLink.href = link
+//    itemText.append(itemLink)
+
+//    const itemDesc = document.createElement('p')
+//    itemDesc.classList.add('item-desc')
+//    itemDesc.innerHTML = desc
+//    itemText.append(itemDesc)
+
+
+//    resultList.append(newItem)
+// }
+
+
+// const createErrorElement = (text) => {
+//    const resultList = document.getElementById('result-items')
+//    let div = document.createElement('div')
+//    div.innerHTML = text
+//    resultList.prepend(div)
+// }
+
+
+
+
+
+// searchButton.addEventListener('click', loadData)
 
 
